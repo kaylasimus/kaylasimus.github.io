59539723b41b4d94386799ea05667dec7a5407e3
kaylasimus <kaylasimus@gmail.com>
2025-11-24 17:58:10 -0800

    Change image after button press; psuedo code reset function (may or may not implement)

--- get_random.js
+++ get_random.js
@@ -1,5 +1,20 @@
+
+// resets buttons
+/*function reset(){
+    let num = document.getElementById('the_number');
+    num.innerText = "";     // blank text
+
+    let react = document.getElementById('result_image');
+    react.src="images/hampter.png";
+
+    let u_button = document.getElementById('user_button');
+    u_button.value="Guess!";
+}
+ */
+
+
 // take in user input to compare
-function random_num(){
+function random_num() {
     // get a random number: whole number((random number * range 0 to x) + 1)
     // + 1 so that instead of * 10 being 0-9, + 1 makes it 1-10
     let random = Math.floor((Math.random() * 10) + 1);
@@ -7,4 +22,12 @@ function random_num(){
     let num = document.getElementById('the_number');
     // assign num to the random number
     num.innerText = random;
+
+    // change image depending on results
+    let react = document.getElementById('result_image');
+    react.src="images/hampter_write.png";
+
+    // change button, invite user to guess again
+    let u_button = document.getElementById('user_button');
+    u_button.value="Guess Again!";
 }
